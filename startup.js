27c9c0edde3b6b0f90ee2659a296813662d17239
skeddles <skeddy13@yahoo.com>
2024-01-21 11:12:21 -0500

    added startup creation of ca-certificate.crt

--- startup.js
+++ startup.js
@@ -1,5 +1,12 @@
 import * as dotenv from 'dotenv';
+import { promises as fs } from 'fs';
 console.log('█'.repeat(80), '\n\nLospec Bot v4 is starting up...');
 console.log('Loading dotenv file...');
 dotenv.config();
+
+if (process.env.MONGO_CERT_FILE) {
+	try {await fs.access('./ca-certificate.crt');}
+	catch (err) {await fs.writeFile('./ca-certificate.crt', process.env.MONGO_CERT_FILE);}
+}
+
 export default true;
