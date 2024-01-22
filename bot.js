a5eab272394ec076bf95dc0968a2a506c819f8fc
skeddles <skeddy13@yahoo.com>
2024-01-22 12:21:52 -0500

    nvm, removed health check

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import startup from "./startup.js";
-import healthCheck from "./health-check.js";
 import commands from './commands.js';
 import responses from './responses.js';
 import data from './data.js';
