d9b48d5000f42e3ad2a485981005427ea371d90a
skeddles <skeddy13@yahoo.com>
2024-01-22 12:07:51 -0500

    added health check bypass

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import startup from "./startup.js";
+import healthCheck from "./health-check.js";
 import commands from './commands.js';
 import responses from './responses.js';
 import data from './data.js';
