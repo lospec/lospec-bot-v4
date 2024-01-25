e6881b76109e7b2d31f7ffd0796616bd959d699b
skeddles <skeddy13@yahoo.com>
2024-01-25 11:44:18 -0500

    art chat heart response now includes embeds, and lospec-dailies channel

--- responses/art-chats-heart-response.js
+++ responses/art-chats-heart-response.js
@@ -4,14 +4,21 @@ import {CONFIG} from '../data.js';
 await CONFIG.assert('heartEmoji');
 
 const ART_CHATS_CATEGORY_NAME = 'Art Chats';
+const LOSPEC_DAILIES_CHANNEL_NAME = 'lospec-dailies';
 
 export const filter = async (message) => {
-	if (message.channel.parent.name != ART_CHATS_CATEGORY_NAME) return false;
-	if (message.attachments.size == 0) return false;
+	let isWithinArtChats = (message.channel.parent.name == ART_CHATS_CATEGORY_NAME);
+	let isInLospecDailiesChannel = (message.channel.name == LOSPEC_DAILIES_CHANNEL_NAME);
+	let hasAttachments = (message.attachments.size > 0);
+	let hasEmbeds = (message.embeds.length > 0);
+
+	if (!isWithinArtChats && !isInLospecDailiesChannel) return false;
+	if (!hasAttachments && !hasEmbeds) return false;
+
 	return true;
 }
 
 export const execute = async (message) => {
-	console.log('adding heart reaction to art-chats message:', 'https://discord.com/channels/'+message.guild.id+'/'+message.channel.id+'/'+message.id);
+	console.log('adding heart reaction to message:', 'https://discord.com/channels/'+message.guild.id+'/'+message.channel.id+'/'+message.id);
 	await message.react(CONFIG.get('heartEmoji'));
 }
