3496b87ba52b5f3ac22934383b0071ff35b43a12
skeddles <skeddy13@yahoo.com>
2024-06-17 14:02:13 -0400

    base command

---
+++ commands/add-emoji.js
@@ -0,0 +1,11 @@
+import { ApplicationCommandType, ApplicationCommandOptionType} from 'discord.js';
+
+export const config = {
+	name: 'add-emoji', 
+	description: 'Add an emoji from the Lospec Emoji Archive to the server (costs 20P)', 
+	type: ApplicationCommandType.ChatInput,
+};
+
+export const execute = async (interaction) => {
+
+};
