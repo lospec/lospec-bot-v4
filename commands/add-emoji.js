ffefb0468a82ce8382819cdae12ae8c19183bbd3
skeddles <skeddy13@yahoo.com>
2024-06-18 14:44:29 -0400

    now checks / takes money

--- commands/add-emoji.js
+++ commands/add-emoji.js
@@ -4,10 +4,13 @@ import fsp from 'fs/promises';
 import path from 'path';
 import { PNG } from 'pngjs';
 import fetch from 'node-fetch';
+import client from '../client.js';
 
 const EMOJI_ARCHIVE_URL = 'https://github.com/lospec/emoji-archive.git';
 const OUTPUT_PATH = '_emoji-archive';
 const PRICE = 20;
+const API_REQUEST_OPTIONS = {headers: {Authorization: process.env.LOZPEKISTAN_BANK_API_KEY}};
+const API_URL = 'http://'+process.env.LOZPEKISTAN_BANK_API_ADDRESS;
 
 try {
 	await fsp.access(OUTPUT_PATH);
@@ -53,7 +56,7 @@ const confirmationActionRow = {
 			type: 2,
 			style: 1,
 			label: 'Add Emoji',
-			customId: 'add_emoji'
+			customId: 'add_emoji_confirm'
 		},
 		{
 			type: 2,
@@ -86,6 +89,7 @@ export const execute = async (interaction) => {
 		title: 'Confirm Purchase',
 		description: 'You are adding the emoji `:'+emojiName+':` to the server. \n\n This will cost you **20P**. \n\n Are you sure you wish to do this?',
 		thumbnail: {url: 'attachment://emoji.png'},
+		author: {name: ':'+emojiName+':'},
 	};
 
 	await interaction.reply({
@@ -120,14 +124,9 @@ async function checkIfUserCanAfford (userId) {
 	let balance;
 
 	try {
-		const response = await fetch('http://'+process.env.LOZPEKISTAN_BANK_API_ADDRESS+'/balance/'+userId, {
-			headers: {
-				Authorization: process.env.LOZPEKISTAN_BANK_API_KEY
-			}
-		});
-		console.log('response:',response);
+		const response = await fetch(API_URL+'/balance/'+userId, API_REQUEST_OPTIONS);
 		const data = await response.json();
-		console.log('user balance:',data);
+		console.log('got user balance:',data);
 		balance = data;
 	}
 	catch (err) {
@@ -137,6 +136,37 @@ async function checkIfUserCanAfford (userId) {
 
 	if (balance > PRICE) return;
 	throw new Error('You do not have enough P to purchase this emoji. You need '+PRICE+'P, but you only have '+balance+'P.');
+}
+
+async function takeUsersMoney (userId) {
+	try {
+		const response = await fetch(API_URL+'/balance/'+userId, API_REQUEST_OPTIONS);
+		const data = await response.json();
+		console.log('got user balance:',data);
+		let balance = data;
+		if (balance < PRICE) 
+			throw new Error('You do not have enough P to purchase this emoji. You need '+PRICE+'P, but you only have '+balance+'P.');
+
+
+		//post request to /balance/userId with body {amount:price}
+		const response2 = await fetch(API_URL+'/balance/'+userId, { 
+			method: 'POST',
+			headers: {
+				...API_REQUEST_OPTIONS.headers, 
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({amount: -PRICE})
+		});
+		if (!response2.ok) 
+			throw new Error('Money taking request failed: '+response2.status, response2.statusText);
+		const data2 = await response2.json();
+
+		console.log('withdrew money:',data2);
+	}
+	catch (err) {
+		console.error('Failed to withdraw money',err);
+		throw new Error('Failed to withdraw money');
+	}
 }
 
 async function scalePng (imagePath, scale=4) {
@@ -167,4 +197,37 @@ async function scalePng (imagePath, scale=4) {
 	}
 
 	return PNG.sync.write(targetPng);
+}
+
+//when the add_emoji_confirm button is clicked
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isButton()) return;
+	if (interaction.customId === 'add_emoji_confirm') 
+		confirmAddEmoji(interaction);
+	else if (interaction.customId === 'cancel')
+		interaction.deleteReply();
+});
+
+async function confirmAddEmoji(interaction) {
+	try {
+		const emojiName = interaction.message.embeds[0].author.name.replace(/:/g,'');
+			console.log('adding emoji:',emojiName);
+		const emojiPath = path.join(OUTPUT_PATH,'current',emojiName+'.png');
+
+		//checks
+		await updateEmojiArchiveToLatest();
+		await checkIfUserCanAfford(interaction.user.id);
+		await checkIfEmojiIsInArchive(emojiPath);
+		await checkIfEmojiIsAlreadyInServer(interaction.guild, emojiName);
+
+		//make it happen
+		await takeUsersMoney(interaction.user.id);
+
+		await interaction.reply({content: "The emoji has been successfully added!", ephemeral: true});
+	}
+	catch (err) {
+		console.log('add emoji request failed:',err);
+		await interaction.reply({content: "Failed to add emoji. " + err.message, ephemeral: true});
+		return;
+	}
 }
