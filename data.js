88ad30b2189ffe4113b211b890dc28531bfd2880
Sam Keddy <skeddy13@yahoo.com>
2025-06-11 08:29:28 -0400

    removed property specific changes

--- data.js
+++ data.js
@@ -7,7 +7,5 @@ else throw new Error('Data storage not configured, please see the "Data Storage"
 export const CONFIG = new Data('config');
 export const YON_DATA = new Data('yon');
 export const YON_CONFIG = new Data('yon-config');
-export const PROPERTY_DATA = new Data('property');
-export const PROPERTY_CONFIG = new Data('propertyconfig');
 export var Data;
 export default true;
--- property-data.js
+++
@@ -1,3 +0,0 @@
-import Data from './data.js';
-export const PROPERTY_DATA = new Data('property');
-export default true;
